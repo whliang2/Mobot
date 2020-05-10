919be42a12367078800a6b27e3ade6e544d2adad
whliang2 <whliang2@illinois.edu>
2020-05-10 16:56:15 -0500

    update

--- README.md
+++ README.md
@@ -15,3 +15,24 @@ Team Members : Teng Yung Lin, Wen Hsuan Liang, Yu Chen Su
 4. Run `python init.py` to construct the folder structure for Mobot project
 5. Run `python main.py` to execute the experiments
 6. The result file will be inside `./data/estimate` folder which shows the model performance of each experiment.
+
+## Dataset :
+1. Index Mundi (https://www.indexmundi.com/) 
+2. World Meter (https://www.worldometers.info/coronavirus/)
+
+## Reference :
+1. Jason Brownlee (2019), Probabilistic Model Selection with AIC, BIC, and MDL, Retrieved from https://machinelearningmastery.com/probabilistic-model-selection-measures/
+2. Greg (2013),Is there a library function for Root mean square error (RMSE) in python?, Retrieved from Stack overflow
+https://stackoverflow.com/questions/17197492/is-there-a-library-function-for-root-mean-square-error-rmse-in-python
+3. Kavindu Dodanduwa (2018), python exception message capturing, Retrieved from Stack overflow
+https://stackoverflow.com/questions/4690600/python-exception-message-capturing
+4. Jason Brownlee (2016), Save and Load Machine Learning Models in Python with scikit-learn, Retrieved from
+https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
+5. Kamon Ayeva, Sakis Kasampalis (2018), Mastering Python Design Patterns - Second Edition
+6. Baran Köseoğlu (2020), Structure Your Data Science Projects - Develop collaborative and reproducible data science projects
+https://towardsdatascience.com/structure-your-data-science-projects-6c6c8653c16a
+7. Bruno Oliveira (2018), pytest Quick Start Guide
+
+
+
+
--- src/estimate/RMSE/RMSE.py
+++ src/estimate/RMSE/RMSE.py
@@ -1,31 +1,39 @@
 import pandas as pd
+import numpy as np
 import sys
+
 sys.path.append('../../../')
 
 from src.utils.Importer.ImporterFactory import ImporterFactory
 from src.utils.Importer.ImporterManager import ImporterManager
-from math import sqrt
-from sklearn.metrics import mean_squared_error
+from numba import jit
+
+
+@jit(nopython=True)
+def cal_rmse(y_pred: np.array, y_true: np.array):
+    result = np.sqrt(np.mean((y_pred - y_true) ** 2))
+    return result
 
 
 class RMSE:
     @staticmethod
     def get_rmse(testing_data: pd.DataFrame, predictors: list, response: list, model):
-            y_pred = model.predict(testing_data[predictors])
-            rmse = sqrt(mean_squared_error(testing_data[response], y_pred))
-            return rmse
+        y_pred = model.predict(testing_data[predictors]).to_numpy()
+        y_true = testing_data[response].to_numpy()
+        rmse = cal_rmse(y_true, y_pred)
+        return rmse
 
 
 if __name__ == '__main__':
     print("### RMSE ###")
 
-     # get source
+    # get source
     source_dir = [{
-            'dir': '../../../data/split/',
-            'files': ['training.csv']
-    },{
-            'dir': '../../../data/split/',
-            'files': ['testing.csv']
+        'dir': '../../../data/split/',
+        'files': ['training.csv']
+    }, {
+        'dir': '../../../data/split/',
+        'files': ['testing.csv']
     }]
     importer = ImporterFactory('csv').generate()
     importer_manager = ImporterManager(importer)
@@ -34,18 +42,22 @@ if __name__ == '__main__':
     # Import Model
     model_files = [{
         'dir': '../../../data/model/models/',
-        'files': ["AIC"]
+        'files': ["SimpleLm"]
     }]
     fetcher_object = ImporterFactory('model').generate()
     model_object = ImporterManager(fetcher_object)
     model_obj = model_object.exec(model_files)[0]
 
     # Test Parameters
-    predictors_list = ['Health.expenditures....of.GDP.', 'Literacy....', 'Physicians.density..physicians.1.000.population.', 'Obesity - adult prevalence rate (%)', 'Life expectancy at birth (years)', 'H_bed_density', 'Imigrate_Rate', 'Pop_Density', 'GDP - per capita (PPP) (US$)']
-    response_list =['Recovery Rate']
+    predictors_list = ['Health.expenditures....of.GDP.', 'Literacy....',
+                       'Physicians.density..physicians.1.000.population.',
+                       'Obesity - adult prevalence rate (%)',
+                       'Life expectancy at birth (years)', 'H_bed_density',
+                       'Imigrate_Rate', 'Pop_Density', 'GDP - per capita (PPP) (US$)',
+                       'Unemployment rate (%)']
+    response_list = ['Recovery Rate']
 
     # Test Class Static Method
     RMSE_Object = RMSE()
     answer = RMSE_Object.get_rmse(testing, predictors_list, response_list, model_obj)
     print(answer)
-
